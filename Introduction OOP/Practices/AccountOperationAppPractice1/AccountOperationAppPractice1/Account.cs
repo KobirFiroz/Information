663a691912835a5d2e67c3cf493547a181ec0f6f
FIROZ <firozCSE38@gmail.com>
2019-06-27 16:52:57 +0600

    Add Information

---
+++ Introduction OOP/Practices/AccountOperationAppPractice1/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    class Account
+    {
+        public string accountnumber { set; get; }
+        public string customername { set; get; }
+       private int balance { set; get; }
+
+        public bool Diposit(int amount)
+        {
+            balance = balance + amount;
+            return true;
+        }
+
+        public bool withdrow(int amount)
+        {
+            if (balance >= 0)
+
+            {
+                balance = balance - amount;
+                return true;
+            }
+
+            else
+            {
+                return false;
+
+            }
+            
+            
+        }
+
+        public string Getreport ()
+        {
+            return customername + "your account number:" + accountnumber + "your balance:" + balance;
+
+        }
+    }
+}
---
+++ Introduction OOP/Practices/AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+          
+        }
+        Account accountobj = new Account();
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            accountobj.customername = customerNameTextBox.Text;
+           accountobj.accountnumber = accountNoTextBox.Text;
+           
+           MessageBox.Show("Successfull");
+
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(amountTextBox.Text);
+            bool isDeposit =   accountobj.Diposit(amount);
+            
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(amountTextBox.Text);
+           bool isWithdrawn = accountobj.withdrow(amount);
+ 
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(accountobj.Getreport());
+        }
+    }
+}
